78efbd865fc63483d456f54c25b0d1385b11da5c
Jack D <jack.dunne4@ucdconnect.ie>
2023-03-28 18:35:19 +0100

    main.c makeover

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
+/*
+ * COMP10050 Software Engineering Assignment 3
+ * Group BQ
+ *      Jack Dunne          22483576
+ *      Maciej Szmagara     (insert student number)
+ * https://csgitlab.ucd.ie/jack-dunne626/bqassignment3.git
+ *
+ */
+
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    printf("Hello, World!\n");
     return 0;
 }
